8de1dab03f1bac254097e110b292f4dc5ca7cb11
Luca Kanyo <lucakanyo@protonmail.com>
2020-05-29 12:37:46 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  def name=(pup)
+    @pup = pup 
+  end 
+  
+  def name 
+    @pup 
+  end 
+  
+  def breed=(breed)
+    @breed = breed 
+  end 
+  
+  def breed 
+    @breed 
+  end 
+end 
